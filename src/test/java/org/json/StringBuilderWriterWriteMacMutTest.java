7d366f913c589c3123b32c00c6bb7c27cca7b393
Vasiliy Mikhailov <vasiliy.mikhailov@gmail.com>
2026-08-01 15:51:14 +0000

    test: improve MAC of src/main/java/org/json/StringBuilderWriter.java::write (round 1)

---
+++ src/test/java/org/json/StringBuilderWriterWriteMacMutTest.java
@@ -0,0 +1,37 @@
+package org.json;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class StringBuilderWriterWriteMacMutTest {
+
+    private StringBuilderWriter writer;
+
+    @Before
+    public void setUp() {
+        writer = new StringBuilderWriter();
+    }
+
+    @Test
+    public void writeCharArrayWithZeroLength() {
+        // covers write:41
+        char[] chars = {'a', 'b', 'c'};
+        writer.write(chars, 0, 0);
+        assertEquals("", writer.toString());
+    }
+
+    @Test
+    public void writeCharArrayWithInvalidOffset() {
+        // covers write:44
+        char[] chars = {'a', 'b', 'c'};
+        try {
+            writer.write(chars, -1, 1);
+            fail("Expected IndexOutOfBoundsException");
+        } catch (IndexOutOfBoundsException e) {
+            // expected
+        }
+    }
+}
