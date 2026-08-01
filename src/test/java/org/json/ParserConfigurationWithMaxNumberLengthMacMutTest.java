b89f57edb932bc6864c5ab616c36b619d2ded9a8
Vasiliy Mikhailov <vasiliy.mikhailov@gmail.com>
2026-08-01 20:57:54 +0000

    test: improve MAC of src/main/java/org/json/ParserConfiguration.java::withMaxNumberLength (round 1)

---
+++ src/test/java/org/json/ParserConfigurationWithMaxNumberLengthMacMutTest.java
@@ -0,0 +1,27 @@
+package org.json;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
+public class ParserConfigurationWithMaxNumberLengthMacMutTest {
+
+    @Test
+    public void withMaxNumberLengthSetsValueWhenPositive() {
+        // covers withMaxNumberLength:170
+        ParserConfiguration config = new ParserConfiguration();
+        ParserConfiguration newConfig = config.withMaxNumberLength(10);
+        assertNotNull(newConfig);
+        assertEquals(10, newConfig.getMaxNumberLength());
+    }
+
+    @Test
+    public void withMaxNumberLengthResetsToUndefinedWhenNegative() {
+        // covers withMaxNumberLength:176
+        ParserConfiguration config = new ParserConfiguration();
+        ParserConfiguration newConfig = config.withMaxNumberLength(-1);
+        assertNotNull(newConfig);
+        assertEquals(ParserConfiguration.UNDEFINED_MAXIMUM_NUMBER_LENGTH, newConfig.getMaxNumberLength());
+    }
+}
