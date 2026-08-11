50e539cc84930b602644a1993aab863053300c3f
Vasiliy Mikhailov <vasiliy.mikhailov@gmail.com>
2026-08-11 08:53:23 +0000

    test: tidy generated tests for src/main/java/org/json/Cookie.java::unescape

--- src/test/java/org/json/CookieUnescapeMacMutTest.java
+++ src/test/java/org/json/CookieUnescapeMacMutTest.java
@@ -8,31 +8,26 @@ public class CookieUnescapeMacMutTest {
 
     @Test
     public void unescapePlusSign() {
-        // covers unescape:200
         assertEquals(" ", Cookie.unescape("+"));
     }
 
     @Test
     public void unescapePercentBoundary() {
-        // covers unescape:203
         assertEquals("%", Cookie.unescape("%"));
     }
 
     @Test
     public void unescapePercentWithOneChar() {
-        // covers unescape:203
         assertEquals("%A", Cookie.unescape("%A"));
     }
 
     @Test
     public void unescapeValidHex() {
-        // covers unescape:203
         assertEquals("A", Cookie.unescape("%41"));
     }
 
     @Test
     public void unescapeInvalidHex() {
-        // covers unescape:203
         assertEquals("%ZZ", Cookie.unescape("%ZZ"));
     }
 }
