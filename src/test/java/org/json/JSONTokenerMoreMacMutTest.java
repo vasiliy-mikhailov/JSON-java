0cec609bf2b488fdab7587782e3b595e17fe0c84
Vasiliy Mikhailov <vasiliy.mikhailov@gmail.com>
2026-08-12 01:37:12 +0000

    test: tidy generated tests for src/main/java/org/json/JSONTokener.java::more

--- src/test/java/org/json/JSONTokenerMoreMacMutTest.java
+++ src/test/java/org/json/JSONTokenerMoreMacMutTest.java
@@ -9,18 +9,12 @@ import org.junit.Test;
 
 public class JSONTokenerMoreMacMutTest {
 
-    // covers more:199
-    // covers more:189
     @Test
     public void moreReturnsFalseAtEOF() {
         JSONTokener tokener = new JSONTokener(new StringReader(""));
         assertFalse(tokener.more());
     }
 
-    // covers more:193
-    // covers more:203
-    // covers more:207
-    // covers more:190
     @Test
     public void moreReturnsTrueWhenContentExists() {
         JSONTokener tokener = new JSONTokener(new StringReader("a"));
